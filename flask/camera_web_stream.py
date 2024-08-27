e55c305faeba65b3fda9540945d4fa9e7c29d58d
poweq <poweq68@gmail.com>
2024-08-27 15:55:03 +0900

    add bluetooh(x) update flask/camera

--- flask/camera_web_stream.py
+++ flask/camera_web_stream.py
@@ -1,41 +1,24 @@
-from flask import Flask, Response
 import cv2
+from flask import Flask, Response
 
 app = Flask(__name__)
+cap = cv2.VideoCapture(0)  # 카메라 장치 열기
 
 def generate_frames():
-    cap = cv2.VideoCapture(0)  # 0은 기본 웹캠을 의미합니다.
     while True:
         success, frame = cap.read()
         if not success:
-            print('not opend camera')
             break
         else:
-            # 비디오 프레임을 JPEG 형식으로 인코딩
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
-            
-            # 프레임을 HTTP 응답 형식으로 전송
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    
-    cap.release()
 
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/')
-def index():
-    return '''
-    <html>
-        <body>
-            <h1>Webcam Feed</h1>
-            <img src="/video_feed">
-        </body>
-    </html>
-    '''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
