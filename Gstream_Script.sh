b23eddf0c02f0949e3e6ec0656d3a61c658a8ff9
poweq <poweq68@gmail.com>
2024-09-20 10:03:54 +0900

    update

--- Gstream_Script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=0.0.0.0 port=5000
-
