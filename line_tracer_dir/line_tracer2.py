5020cf26c96ef4cc4554ec795a9b00a0bc0807a3
poweq <poweq68@gmail.com>
2024-09-20 10:15:13 +0900

    update line_tracer_dir

--- line_tracer_dir/line_tracer2.py
+++ line_tracer_dir/line_tracer2.py
@@ -1,31 +1,31 @@
 import cv2
 import os
-import serial  # 시리얼 통신을 위한 모듈
+import serial
 from jd_opencv_lane_detect import JdOpencvLaneDetect
 
-# 시리얼 포트 설정
+# Serial communication setup
 ser = serial.Serial('/dev/ttyAMA1', 9600, timeout=1)
 
-# OpenCV line detector 객체 생성
+# OpenCV line detector setup
 cv_detector = JdOpencvLaneDetect()
 
-# 카메라 객체 생성
+# Open camera
 cap = cv2.VideoCapture(0)
-cap.set(3, 320)  # 가로 해상도 설정
-cap.set(4, 240)  # 세로 해상도 설정
+cap.set(3, 320)  # Set width
+cap.set(4, 240)  # Set height
 
-# ./data 폴더가 없으면 생성
+# Create directory for saving video if it doesn't exist
 try:
     if not os.path.exists('./data'):
         os.makedirs('./data')
 except OSError:
     print("failed to make ./data folder")
 
-# 비디오 녹화 객체 생성
+# Setup video writer
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 video_orig = cv2.VideoWriter('./data/car_video.avi', fourcc, 20.0, (320, 240))
 
-# 초기화 루틴
+# Initial frame capture
 for i in range(30):
     ret, img_org = cap.read()
     if ret:
@@ -39,14 +39,14 @@ for i in range(30):
     else:
         print("camera error")
 
-# 주행 루틴
+# Main loop
 while True:
     ret, img_org = cap.read()
     if ret:
-        # 비디오 저장
+        # Save the frame to video
         video_orig.write(img_org)
         
-        # 차선 감지
+        # Process the frame
         lanes, img_lane = cv_detector.get_lane(img_org)
         angle, img_angle = cv_detector.get_steering_angle(img_lane, lanes)
         if img_angle is None:
@@ -56,25 +56,25 @@ while True:
             cv2.imshow('lane', img_angle)
             print(angle)
 
-            # 앵글 값에 따라 시리얼 데이터 전송
+            # Send angle as numeric value via serial
             if 80 <= angle <= 100:
-                ser.write(b'w')  # 직진 신호
-                print("Sending 'w' via serial")
+                ser.write(f'{angle}\n'.encode())  # Send angle value as a string
+                print(f"Sending '{angle}' via serial")
             elif 40 <= angle < 80:
-                ser.write(b'l')  # 좌회전 신호
-                print("Sending 'l' via serial")
+                ser.write(f'{angle}\n'.encode())  # Send angle value as a string
+                print(f"Sending '{angle}' via serial")
             elif 101 <= angle <= 120:
-                ser.write(b'r')  # 우회전 신호
-                print("Sending 'r' via serial")
+                ser.write(f'{angle}\n'.encode())  # Send angle value as a string
+                print(f"Sending '{angle}' via serial")
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         print("cap error")
 
-# 리소스 해제
+# Release resources
 cap.release()
 video_orig.release()
 cv2.destroyAllWindows()
-ser.close()  # 시리얼 포트 닫기
+ser.close()
 
